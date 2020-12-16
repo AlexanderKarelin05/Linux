d597d0c2483b724fec19a296480bc2cb176768fd
Makarov <raccoon@example.com>
2020-12-16 17:26:10 +0300

    # add code files.

---
+++ minishell/main.c
@@ -0,0 +1,313 @@
+/*-
+ * main.c
+ * Minishell C source
+ * Shows how to use "obtain_order" input interface function.
+ *
+ * Copyright (c) 1993-2002-2019, Francisco Rosales <frosal@fi.upm.es>
+ * Todos los derechos reservados.
+ *
+ * Publicado bajo Licencia de Proyecto Educativo Práctico
+ * <http://laurel.datsi.fi.upm.es/~ssoo/LICENCIA/LPEP>
+ *
+ * Queda prohibida la difusión total o parcial por cualquier
+ * medio del material entregado al alumno para la realización
+ * de este proyecto o de cualquier material derivado de este,
+ * incluyendo la solución particular que desarrolle el alumno.
+ *
+ * DO NOT MODIFY ANYTHING OVER THIS LINE
+ * THIS FILE IS TO BE MODIFIED
+ */
+
+#include <stddef.h>			/* NULL */
+#include <stdio.h>			/* setbuf, printf */
+#include <stdlib.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAXCOM 1000 // max number of letters to be supported 
+#define MAXLIST 100 // max number of commands to be supported 
+
+extern int obtain_order();		/* See parser.y for description */
+
+// Function to print Current Directory. 
+void printDir() 
+{ 
+	char cwd[1024]; 
+	getcwd(cwd, sizeof(cwd)); 
+	printf("\nCurrent Dir: %s\n", cwd); 
+}
+
+// Function where the system command is executed 
+void execArgs(char** parsed) 
+{ 
+	// Forking a child 
+	pid_t pid = fork(); 
+
+	if (pid == -1) { 
+		printf("\nFailed forking child.."); 
+		return; 
+	}
+	else if (pid == 0) { 
+		if (execvp(parsed[0], parsed) < 0) { 
+			printf("\nCould not execute command.."); 
+		} 
+		exit(0); 
+	}
+	else { 
+		// waiting for child to terminate 
+		wait(NULL); 
+		return; 
+	} 
+} 
+
+// Function where the piped system commands is executed 
+void execArgsPiped(char** parsed, char** parsedpipe) 
+{ 
+	// 0 is read end, 1 is write end 
+	int pipefd[2]; 
+	pid_t p1, p2; 
+
+	if (pipe(pipefd) < 0) { 
+		printf("\nPipe could not be initialized"); 
+		return; 
+	} 
+	p1 = fork(); 
+	if (p1 < 0) { 
+		printf("\nCould not fork"); 
+		return; 
+	} 
+
+	if (p1 == 0) { 
+		// Child 1 executing.. 
+		// It only needs to write at the write end 
+		close(pipefd[0]); 
+		dup2(pipefd[1], STDOUT_FILENO); 
+		close(pipefd[1]); 
+
+		if (execvp(parsed[0], parsed) < 0) { 
+			printf("\nCould not execute command 1.."); 
+			exit(0); 
+		} 
+	}
+	else { 
+		// Parent executing 
+		p2 = fork(); 
+
+		if (p2 < 0) { 
+			printf("\nCould not fork"); 
+			return; 
+		} 
+
+		// Child 2 executing.. 
+		// It only needs to read at the read end 
+		if(p2 == 0) { 
+			close(pipefd[1]); 
+			dup2(pipefd[0], STDIN_FILENO); 
+			close(pipefd[0]); 
+			if (execvp(parsedpipe[0], parsedpipe) < 0) { 
+				printf("\nCould not execute command 2.."); 
+				exit(0); 
+			} 
+		} else { 
+			// parent executing, waiting for two children 
+			wait(NULL); 
+			wait(NULL); 
+		} 
+	} 
+} 
+
+// Help command builtin 
+void openHelp() 
+{ 
+	puts("\n***WELCOME TO MY SHELL HELP***"
+		"\nCopyright @ 2020"
+		"\n-Use the shell at your own risk..."
+		"\nList of Commands supported:"
+		"\n>cd"
+		"\n>ls"
+		"\n>exit"
+		"\n>all other general commands available in UNIX shell"
+		"\n>pipe handling"
+		"\n>improper space handling"); 
+
+	return; 
+} 
+
+// Function to execute builtin commands 
+int ownCmdHandler(char** parsed) 
+{ 
+	int NoOfOwnCmds = 4, i, switchOwnArg = 0; 
+	char* ListOfOwnCmds[NoOfOwnCmds]; 
+	char* username; 
+
+	ListOfOwnCmds[0] = "exit"; 
+	ListOfOwnCmds[1] = "cd"; 
+	ListOfOwnCmds[2] = "help"; 
+	ListOfOwnCmds[3] = "hello"; 
+
+	for (i = 0; i < NoOfOwnCmds; i++) { 
+		if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) { 
+			switchOwnArg = i + 1; 
+			break; 
+		} 
+	} 
+
+	switch (switchOwnArg) { 
+	case 1: 
+		printf("\nGoodbye\n"); 
+		exit(0); 
+	case 2: 
+		chdir(parsed[1]); 
+		return 1; 
+	case 3: 
+		openHelp(); 
+		return 1; 
+	case 4: 
+		username = getenv("USER"); 
+		printf("\nHello %s.\nMind that this is "
+			"not a place to play around."
+			"\nUse help to know more..\n", 
+			username); 
+		return 1; 
+	default: 
+		break; 
+	} 
+
+	return 0; 
+} 
+
+// function for finding pipe 
+int parsePipe(char* str, char** strpiped) 
+{ 
+	int i; 
+	for (i = 0; i < 2; i++) { 
+		strpiped[i] = strsep(&str, "|"); 
+		if (strpiped[i] == NULL) 
+			break; 
+	} 
+
+	if (strpiped[1] == NULL) 
+		return 0; // returns zero if no pipe is found. 
+	else { 
+		return 1; 
+	} 
+} 
+
+// function for parsing command words 
+void parseSpace(char* str, char** parsed) 
+{ 
+	int i; 
+
+	for (i = 0; i < MAXLIST; i++) { 
+		parsed[i] = strsep(&str, " "); 
+
+		if (parsed[i] == NULL) 
+			break; 
+		if (strlen(parsed[i]) == 0) 
+			i--; 
+	} 
+} 
+
+int processString(char* str, char** parsed, char** parsedpipe) 
+{ 
+
+	char* strpiped[2]; 
+	int piped = 0; 
+
+	piped = parsePipe(str, strpiped); 
+
+	if (piped) { 
+		parseSpace(strpiped[0], parsed); 
+		parseSpace(strpiped[1], parsedpipe); 
+
+	}
+	else { 
+
+		parseSpace(str, parsed); 
+	} 
+
+	if (ownCmdHandler(parsed)) 
+		return 0; 
+	else
+		return 1 + piped; 
+} 
+
+int main(void)
+{
+	char ***argvv = NULL;
+	int argvc;
+	char **argv = NULL;
+	int argc;
+	char *filev[3] = { NULL, NULL, NULL };
+	int bg;
+	int ret;
+	
+	char inputString[MAXCOM], *parsedArgs[MAXLIST]; 
+	char* parsedArgsPiped[MAXLIST]; 
+	int execFlag = 0;
+	char tmp[100]; 
+
+	setbuf(stdout, NULL);			/* Unbuffered */
+	setbuf(stdin, NULL);
+
+	while (1) {
+		fprintf(stderr, "%s", "msh> ");	/* Prompt */
+		ret = obtain_order(&argvv, filev, &bg);
+		if (ret == 0) break;		/* EOF */
+		if (ret == -1) continue;	/* Syntax error */
+		argvc = ret - 1;		/* Line */
+		if (argvc == 0) continue;	/* Empty line */
+#if 1
+/*
+ * LAS LINEAS QUE A CONTINUACION SE PRESENTAN SON SOLO
+ * PARA DAR UNA IDEA DE COMO UTILIZAR LAS ESTRUCTURAS
+ * argvv Y filev. ESTAS LINEAS DEBERAN SER ELIMINADAS.
+ */
+ 		// print shell line 
+		//printDir();
+		
+		for (argvc = 0; (argv = argvv[argvc]); argvc++) {
+			for (argc = 0; argv[argc]; argc++){							
+				sprintf(tmp, "%s ", argv[argc]);		
+			}
+			
+			strcpy(inputString, *argv);
+			if (argc > 1)
+			{
+				strcat(inputString, " ");
+				strcat(inputString, tmp);
+			}
+			
+			//printf("%s ",inputString);
+			//printf("\n");
+			
+			execFlag = processString(inputString, 
+			parsedArgs,
+			parsedArgsPiped); 
+			// execflag returns zero if there is no command 
+			// or it is a builtin command, 
+			// 1 if it is a simple command 
+			// 2 if it is including a pipe. 
+
+			// execute 
+			if(execFlag == 1) 
+				execArgs(parsedArgs); 
+
+			if (execFlag == 2) 
+				execArgsPiped(parsedArgs, parsedArgsPiped);			
+		}		
+		
+		if (filev[0]) printf("< %s\n", filev[0]);/* IN */
+		if (filev[1]) printf("> %s\n", filev[1]);/* OUT */
+		if (filev[2]) printf(">& %s\n", filev[2]);/* ERR */
+		if (bg) printf("&\n");
+/*
+ * FIN DE LA PARTE A ELIMINAR
+ */
+#endif
+	}
+	exit(0);
+	return 0;
+}
